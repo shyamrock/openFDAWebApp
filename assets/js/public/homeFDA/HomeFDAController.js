248c3303b6c204d2a2fd70b1a29267437e196181
Shyam Nagarajan <root>
2015-06-22 20:52:07 -0500

    testing CI

--- assets/js/public/homeFDA/HomeFDAController.js
+++ assets/js/public/homeFDA/HomeFDAController.js
@@ -1,4 +1,4 @@
 angular.module('openFDA').controller('HomeFDAController', function($scope){
 
-$scope.message ="Cool portal with Jenkins test";
+$scope.message ="openFDA Web App with CI Jenkins test";
 });
