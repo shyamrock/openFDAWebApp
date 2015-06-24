e8db41f91a2aca99455a15cef6f2eeecd7c288f9
Shyam Nagarajan <root>
2015-06-24 07:01:45 -0500

    commiting to set the modules correct

--- config/connections.js
+++ config/connections.js
@@ -58,11 +58,11 @@ module.exports.connections = {
   ***************************************************************************/
   someMongodbServer: {
     adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
+    host: 'ds031611.mongolab.com',
+    port: 31611,
+    user: 'adminfda',
+    password: 'openfda2015!',
+    database: 'openfdadb'
   },
 
   /***************************************************************************
--- config/models.js
+++ config/models.js
@@ -11,13 +11,14 @@
 
 module.exports.models = {
 
+
   /***************************************************************************
   *                                                                          *
   * Your app's default connection. i.e. the name of one of your app's        *
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
-  // connection: 'localDiskDb',
+   connection: 'someMongodbServer',
 
   /***************************************************************************
   *                                                                          *
@@ -27,6 +28,6 @@ module.exports.models = {
   * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
   *                                                                          *
   ***************************************************************************/
-  // migrate: 'alter'
+   migrate: 'alter'
 
 };
