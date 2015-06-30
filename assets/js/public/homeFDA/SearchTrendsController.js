32fe7abe153be0d33a6647c0fb6a273fc81eee74
Vimalkovath <vimal.kovath@gmail.com>
2015-06-30 17:41:41 +0530

    trends page, logo on left, color change,pagination,

--- assets/js/public/homeFDA/SearchTrendsController.js
+++ assets/js/public/homeFDA/SearchTrendsController.js
@@ -1,4 +1,4 @@
-angular.module('openFDA').controller('SearchTrendsController', function($scope, openFDASearchTrendsService, $location, $filter) {
+angular.module('openFDA').controller('SearchTrendsController', function($scope, openFDASearchTrendsService, $location, $filter,NgTableParams, $sce) {
   init();
 
 
@@ -6,7 +6,28 @@ angular.module('openFDA').controller('SearchTrendsController', function($scope, 
 
     openFDASearchTrendsService.getFeedbackPaged().then(function(data){
       $scope.searchTrendsResult=data;
-      $scope.itemsByPage=10;
+
+
+      var dataa= $scope.searchTrendsResult.data;
+      $scope.trends=dataa;
+      $scope.tableParams = new NgTableParams({
+        page: 1,            // show first page
+        count: 10           // count per page
+      }, {
+        total: dataa.length, // length of data
+        getData: function($defer, params) {
+          var orderedData = params.sorting ?
+            $filter('orderBy')(dataa, params.orderBy()) :
+            dataa;
+          orderedData = params.filter ?
+            $filter('filter')(orderedData, params.filter()) :
+            orderedData;
+
+
+          $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        }
+      });
+
     });
 
   }
