3b84ee08eea853bb24e47431447e9a9f5e67c6f7
deepaksabu <deepaksabu102@gmail.com>
2015-07-01 17:18:50 +0530

    chart

--- assets/js/public/homeFDA/HomeFDAController.js
+++ assets/js/public/homeFDA/HomeFDAController.js
@@ -158,8 +158,8 @@ $scope.message ="openFDA Data Set View";
 
 
   $scope.itemsByPage=10;
-
-  $scope.options = {
+  alert("adjlaskdf")
+  $scope.option = {
     data: [
       {
         sales: 130,
@@ -189,5 +189,22 @@ $scope.message ="openFDA Data Set View";
       $scope.gridApi = gridApi;
     }
   };
+
+  var container = document.createElement('div');
+  var myEl = angular.element( document.querySelector( '#chart2' ) );
+  myEl.append(container);
+  window.chart = new Highcharts.Chart({
+    chart: {
+      renderTo: container,
+      height: 400
+    },
+    xAxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+
+    series: [{
+      data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+    }]
+  });
 
 });
