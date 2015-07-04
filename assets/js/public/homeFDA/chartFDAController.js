816235be4147f3a8c1ff1d99492e923e16782c43
deepaksabu <deepaksabu102@gmail.com>
2015-07-04 18:40:16 +0530

    chart yearwise series

--- assets/js/public/homeFDA/chartFDAController.js
+++ assets/js/public/homeFDA/chartFDAController.js
@@ -24,7 +24,7 @@ angular.module('openFDA').controller('ChartController', function($scope, $locati
       url: url, method: "GET",
       params: {selectType: selectType,searchItem:searchItem}
     }).
-           success(function (data, status, headers, config) {
+           /*success(function (data, status, headers, config) {
         $scope.result = data.results;
         var rnd = [];
 
@@ -43,6 +43,85 @@ angular.module('openFDA').controller('ChartController', function($scope, $locati
           rnd.push([x, result.count]);
         });
         $scope.chartConfig.series.push({data: rnd});
+      });*/
+
+      success(function(data, status, headers, config) {
+        $scope.result = data.results;
+        var rnd = [];
+        var rnd1 = [];
+        var rnd2 = [];
+        var rnd3 = [];
+        $scope.chartConfig.title.text= $scope.selectType+'Recall with '+$scope.searchItem+' for Reason for Recall'
+        $scope.formatedVmlAray=[   { name: "2012",data:[]}
+
+        ]
+
+
+        angular.forEach($scope.result, function (result) {
+          var vml = [];
+          var datetime1 = result.time
+
+          //  vml.push( $scope.vml);
+
+          var yyyy = datetime1.substring(0, 4),mm = datetime1.substring(4, 6), dd = datetime1.substring(6, 8),datejoined = yyyy + "," + mm + "," + dd;
+          var year='2015'
+          var x = Date.UTC(year,mm, dd);
+
+          var i=0;
+          if(yyyy==='2012')
+          {
+            rnd.push([x, result.count]);
+          }
+          else if(yyyy==='2013')
+          {
+            rnd1.push([x, result.count]);
+          }
+          else if(yyyy==='2014')
+          {
+            rnd2.push([x, result.count]);
+          }
+          else
+          {
+            rnd3.push([x, result.count]);
+          }
+          /*if(yyyy==='2012')
+           {
+           angular.forEach($scope.formatedVmlAray, function (array) {
+           if(array.name==='2012')
+           {
+           $scope.formatedVmlAray.push(            {    data: [   [Date.UTC(1970, 10, i+1), ],
+           [Date.UTC(1970, 11, 4), 0.28],
+           [Date.UTC(1971, 10, 9), 0.25]
+           ]
+           }
+           );
+
+           //alert("its matching");
+           }
+
+
+           });
+
+           //alert(yyyy);
+
+           }*/
+
+          //rnd.push([x, result.count]);
+          //$scope.chartConfig.series.push( $scope.formatedVmlAray[0]);
+        });
+        //$scope.formatedVmlAray=[];
+        //$scope.formatedVmlAray.push({ name: "Winter 2012-2013",data: [   [Date.UTC(1973, 9, 21), 0],[Date.UTC(1973, 10, 4), 0.28], [Date.UTC(1973, 10, 9), 0.25]      ] })
+        //$scope.formatedVmlAray.push({ name: "Winter 2013-2014",data: [   [Date.UTC(1972, 9, 01), 4],[Date.UTC(1972, 10, 7), 0.58], [Date.UTC(1972, 10, 7), 0.55]      ] })
+        //$scope.formatedVmlAray.push({ name: "Winter 2013-2014",data: [   [Date.UTC(1972, 10, 11), 4],[Date.UTC(1972, 1, 7), 0.98], [Date.UTC(1972, 10, 7), 0.55]      ] })
+        //
+        //  $scope.chartConfig.series.push( $scope.formatedVmlAray[0]);
+        //$scope.chartConfig.series.push( $scope.formatedVmlAray[1]);
+        //$scope.chartConfig.series.push( $scope.formatedVmlAray[2]);
+        $scope.chartConfig.series=[];
+        $scope.chartConfig.series.push( {name:"2012",data:rnd});
+        $scope.chartConfig.series.push( {name:"2013",data:rnd1});
+        $scope.chartConfig.series.push( {name:"2014",data:rnd2});
+        $scope.chartConfig.series.push( {name:"2015",data:rnd3});
       });
 
   }
@@ -57,6 +136,9 @@ angular.module('openFDA').controller('ChartController', function($scope, $locati
     success(function(data, status, headers, config) {
       $scope.result = data.results;
       var rnd = [];
+      var rnd1 = [];
+      var rnd2 = [];
+      var rnd3 = [];
           $scope.formatedVmlAray=[   { name: "2012",data:[]}
 
                                   ]
@@ -69,9 +151,26 @@ angular.module('openFDA').controller('ChartController', function($scope, $locati
         //  vml.push( $scope.vml);
 
         var yyyy = datetime1.substring(0, 4),mm = datetime1.substring(4, 6), dd = datetime1.substring(6, 8),datejoined = yyyy + "," + mm + "," + dd;
-        var x = Date.UTC(yyyy, mm, dd);
+        var year='2015'
+          var x = Date.UTC(year,mm, dd);
 
           var i=0;
+          if(yyyy==='2012')
+          {
+            rnd.push([x, result.count]);
+          }
+          else if(yyyy==='2013')
+          {
+            rnd1.push([x, result.count]);
+          }
+          else if(yyyy==='2014')
+          {
+            rnd2.push([x, result.count]);
+          }
+          else
+          {
+            rnd3.push([x, result.count]);
+          }
           /*if(yyyy==='2012')
           {
             angular.forEach($scope.formatedVmlAray, function (array) {
@@ -94,7 +193,7 @@ angular.module('openFDA').controller('ChartController', function($scope, $locati
 
           }*/
 
-        rnd.push([x, result.count]);
+        //rnd.push([x, result.count]);
           //$scope.chartConfig.series.push( $scope.formatedVmlAray[0]);
       });
       //$scope.formatedVmlAray=[];
@@ -105,7 +204,10 @@ angular.module('openFDA').controller('ChartController', function($scope, $locati
     //  $scope.chartConfig.series.push( $scope.formatedVmlAray[0]);
       //$scope.chartConfig.series.push( $scope.formatedVmlAray[1]);
       //$scope.chartConfig.series.push( $scope.formatedVmlAray[2]);
-      $scope.chartConfig.series.push( {data:rnd});
+      $scope.chartConfig.series.push( {name:"2012",data:rnd});
+      $scope.chartConfig.series.push( {name:"2013",data:rnd1});
+      $scope.chartConfig.series.push( {name:"2014",data:rnd2});
+      $scope.chartConfig.series.push( {name:"2015",data:rnd3});
     });
 
 
@@ -150,25 +252,30 @@ angular.module('openFDA').controller('ChartController', function($scope, $locati
     options: {
       chart: {
         type: 'spline'
-      }
+      },
+      tooltip: {
+        headerFormat:'',
+        pointFormat: '{point.x:%e.%B }: {point.y:.2f} '
+      },
+
     },
 
     yAxis: {
       title: {
         text: "COUNT"
-      }},
+      }
+    },
 
       xAxis: {
         dateTimeLabelFormats: { // don't display the dummy year
-          month: '%e \ %b \'%y',
-          year: '%Y'
-          //month: '%e. %b',
-          //year: '%b'
+          month: '%e.%B ',
+          /*year: ['%y']*/
         },
         type: 'datetime',
         title: {
           text: "DATE/YEAR"
-        }
+        },
+
       },
     series:[ ],
     title: {
@@ -188,7 +295,18 @@ angular.module('openFDA').controller('ChartController', function($scope, $locati
 //  $scope.chartConfig.series.push($scope.serieses[i]);
 //}
 
-
+  $scope.swapChartTypeBar = function () {
+    this.chartConfig.options.chart.type = 'bar'
+    this.chartConfig.options.chart.zoomType = 'y'
+  }
+  $scope.swapChartTypeLine = function () {
+    this.chartConfig.options.chart.type = 'line'
+    this.chartConfig.options.chart.zoomType = 'x'
+  }
+  $scope.swapChartTypeSpline = function () {
+    this.chartConfig.options.chart.type = 'spline'
+    this.chartConfig.options.chart.zoomType = 'x'
+  }
 
 
 
